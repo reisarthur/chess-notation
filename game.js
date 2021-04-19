c8e2cd72f2f0c2bde347e312adf63ff7986e4545
Arthur Reis <arthur.reis@engenharia.ufjf.br>
2021-04-19 17:22:07 -0300

    Initial Commit

---
+++ game.js
@@ -0,0 +1,36 @@
+$("#square-random").click(function () {
+    $("#square-random").text(randomSquare);
+});
+
+$(".chess-square").click(function () {
+    $("#square-clicked").text($(this).attr('id'));
+});
+
+$("#btn-squarenames").click(function () {
+    $(".notation").toggleClass('hidden');
+    $(this).text(function(i, text){
+          return text === "Show square names" ? "Hide square names" : "Show square names";
+      })
+});
+
+$("#btn-reverse").click(function () {
+    $(this).text(function(i, text){
+          if (text === "White on bottom") {
+            $(".chess-board").css('flex-direction','column');
+            $(".chess-row").css('flex-direction','row');
+            return "Black on bottom";
+          } else {
+            $(".chess-board").css('flex-direction','column-reverse');
+            $(".chess-row").css('flex-direction','row-reverse');
+            return "White on bottom";
+          }
+      })
+});
+
+function randomSquare() {
+  var randomNumber = Math.floor(Math.random() * 64);
+  var col = Math.floor(randomNumber / 8);
+  var row = randomNumber % 8 + 1;
+  var square = String.fromCharCode(97 + col) + row;
+  return square;
+}
